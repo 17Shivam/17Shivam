19fe72e9fdc35dffa0b9d30ae5f8118e4ece9218
17Shivam <130686924+17Shivam@users.noreply.github.com>
2023-08-27 04:11:33 +0530

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+hi i am @17Shivam
+- 👀 I’m interested in Data science $AI.
+- 🌱 I’m currently learning Data science,ML as well as AI. ...
+- 💞️ I’m looking to collaborate on  several projects. 
+- 📫 How to reach me ...
+
+<!---
+17Shivam/17Shivam is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
